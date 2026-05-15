dd706bab4cdf13c7ecc81c12003d9dad0533e3ed
Abdoz <abdelrhmansalah108@gmail.com>
2026-05-15 18:59:05 +0300

    feat: M1 - design system foundation (colo9rs, fonts, dark mode)

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -32,4 +32,15 @@ class AppColors {
     begin: Alignment.topCenter,
     end: Alignment.bottomCenter,
   );
+
+  //-----Surfanes----------------------------------------------
+  static const Color surfaceDark = Color(0xff1c1b1b);
+  static const Color surfaceContainerLowDark = Color(0xff252323);
+  static const Color surfaceContinerHighDark = Color(0xff2e2c2);
+  static const Color surfaceContainerHighestDark = Color(0xff383535);
+
+  //----Neutrals (Dark) ----------------------------------------
+  static const Color onSurfaceDark = Color(0xfff3efed);
+  static const Color onSurfaceVarianDark = Color(0xffada8a6);
+  
 }
--- lib/theme/typography.dart
+++ lib/theme/typography.dart
@@ -10,21 +10,21 @@ class AppTypography {
     fontSize: 36,
     fontWeight: FontWeight.w700,
     height: 1.15,
-    color: AppColors.deepNeutral,
+    // color: AppColors.deepNeutral,
   );
 
   static TextStyle get displayMd => GoogleFonts.newsreader(
     fontSize: 28,
     fontWeight: FontWeight.w600,
     height: 1.2,
-    color: AppColors.deepNeutral,
+    // color: AppColors.deepNeutral,
   );
 
   static TextStyle get headline => GoogleFonts.newsreader(
     fontSize: 22,
     fontWeight: FontWeight.w600,
     height: 1.3,
-    color: AppColors.deepNeutral,
+    // color: AppColors.,
   );
   static TextStyle get headlineColor => GoogleFonts.newsreader(
     fontSize: 22,
@@ -38,14 +38,14 @@ class AppTypography {
     fontWeight: FontWeight.w400,
     fontStyle: FontStyle.italic,
     height: 1.5,
-    color: AppColors.deepNeutral,
+    // color: AppColors.deepNeutral,
   );
 
   static TextStyle get bodySerif => GoogleFonts.newsreader(
     fontSize: 17,
     fontWeight: FontWeight.w400,
     height: 1.7,
-    color: AppColors.deepNeutral,
+    // color: AppColors.deepNeutral,
   );
 
   //--Work Sans (Sans-serif)---UIU & Metadata -----------------
@@ -54,49 +54,49 @@ class AppTypography {
     fontSize: 18,
     fontWeight: FontWeight.w600,
     height: 1.3,
-    color: AppColors.deepNeutral,
+    // color: AppColors.deepNeutral,
   );
   static TextStyle get titleMd => GoogleFonts.workSans(
     fontSize: 16,
     fontWeight: FontWeight.w500,
     height: 1.4,
-    color: AppColors.deepNeutral,
+    // color: AppColors.deepNeutral,
   );
 
   static TextStyle get body => GoogleFonts.workSans(
     fontSize: 16,
     fontWeight: FontWeight.w500,
     height: 1.4,
-    color: AppColors.deepNeutral,
+    // color: AppColors.deepNeutral,
   );
   static TextStyle get labelLg => GoogleFonts.workSans(
     fontSize: 13,
     fontWeight: FontWeight.w500,
     height: 1.4,
-    color: AppColors.deepNeutral,
+    // color: AppColors.deepNeutral,
   );
   static TextStyle get labelMd => GoogleFonts.workSans(
     fontSize: 12,
     fontWeight: FontWeight.w500,
     height: 1.4,
-    color: AppColors.deepNeutral,
+    // color: AppColors.deepNeutral,
   );
   static TextStyle get categoryTag => GoogleFonts.workSans(
     fontSize: 11,
     fontWeight: FontWeight.w600,
     height: 0.10,
-    color: AppColors.deepNeutral,
+    // color: AppColors.deepNeutral,
   ).copyWith();
   static TextStyle get buttonLabel => GoogleFonts.workSans(
     fontSize: 14,
     fontWeight: FontWeight.w600,
     height: 0.04,
-    color: AppColors.onPrimary,
+    // color: AppColors.onPrimary,
   );
   static TextStyle get caption => GoogleFonts.workSans(
     fontSize: 11,
     fontWeight: FontWeight.w400,
     // height: 1.3,
-    color: AppColors.onSurfaceVariant,
+    // color: AppColors.onSurfaceVariant,
   );
 }
