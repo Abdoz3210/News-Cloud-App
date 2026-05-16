5ce5d3dc3d18e6eae98022c8d514f1576a85ccbf
Abdoz <abdelrhmansalah108@gmail.com>
2026-05-16 20:41:17 +0300

    feat: Firebase setup and splash screen fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:news_app/core/providers/theme_providers.dart';
 import 'package:news_app/views/design_test_views.dart';
 import 'package:news_app/views/home_views.dart';
 import 'package:news_app/views/main_view.dart';
 import 'package:provider/provider.dart';
 import 'theme/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:news_app/firebase_options.dart';
 
 final dio = Dio();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // FlutterNativeSplash.preserve(widgetsBinding: WidgetsBinding.instance);
+
   runApp(
     ChangeNotifierProvider(
       create: (_) => ThemeProviders()..loadTheme(),
