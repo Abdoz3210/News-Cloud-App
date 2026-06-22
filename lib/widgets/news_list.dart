635688b4cafa3fe73024ff78d384f467a177fee1
Abdoz <abdelrhmansalah108@gmail.com>
2026-06-22 06:55:57 +0300

    feat: M3 — full editorial News Detail screen with author bio
    Closes #23

--- lib/widgets/news_list.dart
+++ lib/widgets/news_list.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/Models/articales_model.dart';
-import 'package:news_app/views/news_detail_view.dart';
 import 'package:news_app/widgets/hero_article_card.dart';
 
 class NewsList extends StatelessWidget {
